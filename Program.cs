a84d4b5b6b8e0d58e8c8aa2db15ba4c3036a9c49
Anup Kagalkar <anupkagalkar@gmail.com>
2020-09-04 15:45:13 -0400

    This is a test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstGitHubProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Anup SIT");
+            Console.WriteLine("Hello Anup SIT  1");
         }
     }
 }
